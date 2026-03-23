d07dbfe73a80aa92f7045400d63e90b62db25b23
Jerry Zheng <Platypus.0905311287@gmail.com>
2026-03-23 17:52:10 +0800

    Deploy the website using Gunicorn.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,7 @@ def hello_api():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
--- worklog.md
+++ worklog.md
@@ -3,3 +3,5 @@
 ## 2026/03/23
 
 - MVP flask.
+
+- Deploy the website using Gunicorn.
